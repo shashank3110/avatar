b9f47c53f6b6fcd35ef16d60ab2c47fa79f5c6bc
shashank3110 <shashank311095@gmail.com>
2021-03-24 00:52:10 +0100

    updated git ignore and requirements file.

--- project_avatar/project_avatar/settings.py
+++ project_avatar/project_avatar/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,5 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())

871b18937df8e9c576b1290efdc766a29bd5c27e
shashank3110 <shashank311095@gmail.com>
2021-03-24 19:35:14 +0100

    updated settings file and add environ var as django secret key.

--- project_avatar/project_avatar/settings.py
+++ project_avatar/project_avatar/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iy+@y*^&y6#l)7o_s+@i$gt_@x@2nw_+gukd@0@tgjljg9bv)w'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_AVATAR')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

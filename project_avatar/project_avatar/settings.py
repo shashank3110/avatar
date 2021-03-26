ae3be81fe5bb658038cc33102c964d636b812cff
shashank3110 <shashank311095@gmail.com>
2021-03-26 01:31:33 +0100

    Portfolio deployed to beanstalk.

--- project_avatar/project_avatar/settings.py
+++ project_avatar/project_avatar/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from pathlib import Path
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_AVATAR')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost','my-openbook-shashank.eu-central-1.elasticbeanstalk.com']
 
 # Application definition
 
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'project_avatar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3') #BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -117,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = 'static'
 
 STATIC_URL = '/static/'
